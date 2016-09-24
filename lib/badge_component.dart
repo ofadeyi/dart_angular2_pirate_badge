cc03e9ae797bf90d4ef11ea63f5bd62390dcf416
ofadeyi <ofadeyi@gmail.com>
2016-09-24 13:31:12 +0100

    injecting the 'name service'

--- lib/badge_component.dart
+++ lib/badge_component.dart
@@ -1,21 +1,33 @@
 import 'package:angular2/core.dart';
+import 'name_service.dart';
 
 @Component(
     selector: 'pirate-badge',
     templateUrl: 'badge_component.html',
-    styleUrls: const ['badge_component.css']
+    styleUrls: const ['badge_component.css'],
+    providers: const [NameService]
 )
 class BadgeComponent {
   String badgeName = '';
   String buttonText = 'Aye! Gimme a name!';
   bool isButtonEnabled = true;
+  final NameService _nameService;
 
-  void generateBadge() {
-    badgeName = 'Anne Bonney';
+  BadgeComponent(this._nameService);
+
+  void setBadgeName([String newName = '']) {
+    if (newName == null) return;
+    badgeName = _nameService.getPirateName(newName);
   }
 
-  void updateBadge(String inputName){
-    if (inputName.trim().isEmpty) {
+  void generateBadge() {
+    setBadgeName();
+  }
+
+  void updateBadge(String inputName) {
+    if (inputName
+        .trim()
+        .isEmpty) {
       buttonText = 'Aye! Gimme a name!';
       isButtonEnabled = true;
     } else {
