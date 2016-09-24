d794364c06cec10440b2b670a7986570599f9798
ofadeyi <ofadeyi@gmail.com>
2016-09-24 12:23:41 +0100

    added badge component

---
+++ lib/badge_component.dart
@@ -0,0 +1,10 @@
+import 'package:angular2/core.dart';
+
+@Component(
+    selector: 'pirate-badge',
+    templateUrl: 'badge_component.html',
+    styleUrls: const ['badge_component.css']
+)
+class BadgeComponent {
+  String badgeName = 'Joe';
+}
